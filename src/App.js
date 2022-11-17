593af888c9d7c26b66799772c7453a91cf2f5c89
Vishnupriya S <smilewithvishnu@gmail.com>
2022-11-17 15:10:08 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,89 @@
+import "./index.css";
+import Card from "./component/Card";
+import React from "react";
+
+function App() {
+  //giving the details of three cards
+  let plans = [
+    {
+      plan: "FREE",
+      price: 0,
+      user: "Single User",
+      userEnabler: true,
+      storage: "5GB Storage",
+      storageEnabler: true,
+      publicProject: "Unlimited public projects",
+      publicProjectEnabler: true,
+      communityAccess: "Community Access",
+      communityAccessEnabler: true,
+      privateProjects: "Unlimited Private Projects",
+      privateProjectsEnabler: false,
+      phoneSupport: "Dedicated Phone Support",
+      phoneSupportEnabler: false,
+      subDomain: "Unlimited Free Subdomains",
+      subDomainEnabler: false,
+      reports: "Monthly Status Reports",
+      reportsEnabler: false,
+    },
+
+    {
+      plan: "PLUS",
+      price: 9,
+      user: "5 Users",
+      userEnabler: true,
+      storage: "5GB Storage",
+      storageEnabler: true,
+      publicProject: "Unlimited public projects",
+      publicProjectEnabler: true,
+      communityAccess: "Community Access",
+      communityAccessEnabler: true,
+      privateProjects: "Unlimited Private Projects",
+      privateProjectsEnabler: true,
+      phoneSupport: "Dedicated Phone Support",
+      phoneSupportEnabler: true,
+      subDomain: "Unlimited Free Subdomains",
+      subDomainEnabler: true,
+      reports: "Monthly Status Reports",
+      reportsEnabler: false,
+    },
+
+    {
+      plan: "PRO",
+      price: 49,
+      user: "Unlimited Users",
+      userEnabler: true,
+      storage: "150GB Storage",
+      storageEnabler: true,
+      publicProject: "Unlimited public projects",
+      publicProjectEnabler: true,
+      communityAccess: "Community Access",
+      communityAccessEnabler: true,
+      privateProjects: "Unlimited Private Projects",
+      privateProjectsEnabler: true,
+      phoneSupport: "Dedicated Phone Support",
+      phoneSupportEnabler: true,
+      subDomain: " Free Subdomains",
+      subDomainEnabler: true,
+      reports: "Monthly Status Reports",
+      reportsEnabler: true,
+    },
+  ];
+
+  return (
+    <div>
+      <section className="pricing py-5">
+        <div className="container">
+          <div className="row">
+            {/* calling the card function by passing the values as data */}
+            <Card data={plans[0]} />
+            <Card data={plans[1]} />
+            <Card data={plans[2]} />
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+//exporting the app function
+export default App;
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+  {/* calling the App function  from App file */}
+    <App />
+  </React.StrictMode>
+);
